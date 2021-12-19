cabe56ef38b34e8a1c3388bed31db120b839cc26
Arslan Assankulov <arslan.assankulov@gmail.com>
2021-12-19 13:46:19 +0600

    Initial Commit

---
+++ HW8A/HW8AApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW8AApp.swift
+//  HW8A
+//
+//  Created by Арслан on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW8AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
